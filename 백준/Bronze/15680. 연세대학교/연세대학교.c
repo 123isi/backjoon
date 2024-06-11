9c11354cba277eb3b344d3ea5dd840d07e2c87cc
123isi <148470980+123isi@users.noreply.github.com>
2024-06-11 10:45:37 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+int main(void) {
+    int x;
+    scanf("%d",&x);
+    if(x==0){
+        printf("YONSEI");
+    }
+    else{
+        printf("Leading the Way to the Future");
+    }
+}
