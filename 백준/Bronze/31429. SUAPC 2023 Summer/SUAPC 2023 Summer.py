423b711597fd9ded40ceeef54c4a2c8f0b9fd796
123isi <148470980+123isi@users.noreply.github.com>
2024-06-24 17:17:26 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.py
@@ -0,0 +1,23 @@
+x=int(input())
+if x==1:
+    print(12,1600)
+elif x==2:
+    print(11,894)
+elif x==3:
+    print(11,1327)
+elif x==4:
+    print(10,1311)
+elif x==5:
+    print(9,1004)
+elif x==6:
+    print(9,1178)
+elif x==7:
+    print(9,1357)
+elif x==8:
+    print(8,837)
+elif x==9:
+    print(7,1055)
+elif x==10:
+    print(6,556)
+elif x==11:
+    print(6,773)
