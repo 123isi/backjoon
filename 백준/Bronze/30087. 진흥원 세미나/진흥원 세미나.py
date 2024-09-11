6829e42fffdfb3b020d5f8df496f1266492a5ef9
123isi <148470980+123isi@users.noreply.github.com>
2024-09-11 09:52:46 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,17 @@
+x=int(input())
+for i in range(x):
+    y=input()
+    if y=="Algorithm":
+        print(204)
+    elif y=="DataAnalysis":
+        print(207)
+    elif y=="ArtificialIntelligence":
+        print(302)
+    elif y=="CyberSecurity":
+        print("B101")
+    elif y=="Network":
+        print("303")
+    elif y=="Startup":
+        print("501")
+    elif y=="TestStrategy":
+        print("105")
