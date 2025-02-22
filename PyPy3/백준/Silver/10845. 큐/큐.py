e763a07af81dd377f9b64c99d63aa11b5f8495a1
이효준 <148470980+123isi@users.noreply.github.com>
2025-02-22 13:26:21 +0900

    [Silver IV] Title: 큐, Time: 412 ms, Memory: 116432 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,21 @@
+from collections import deque
+import sys
+
+li = deque()
+x = int(input())
+
+for _ in range(x):
+    a = input().strip().split()
+    
+    if a[0] == "push":
+        li.append(int(a[1]))
+    elif a[0] == "front":
+        print(li[0] if li else -1)
+    elif a[0] == "back":
+        print(li[-1] if li else -1)
+    elif a[0] == "pop":
+        print(li.popleft() if li else -1)
+    elif a[0] == "size":
+        print(len(li))
+    elif a[0] == "empty":
+        print(1 if not li else 0)
