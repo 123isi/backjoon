d112c1aea0e50dfe0bd1e5457a0d0ee2272c953d
이효준 <148470980+123isi@users.noreply.github.com>
2025-03-26 00:08:37 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 436 ms, Memory: 122872 KB -BaekjoonHub

--- PyPy3/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ PyPy3/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -1,8 +1,8 @@
-x = int(input())
-li = []
-for i in range(x):
-    a, b = map(int, input().split())
-    li.append([a, b])
-li.sort(key=lambda y: (y[1],y[0]))
+a=int(input())
+li=[]
+for i in range(a):
+    x,y=map(int,input().split())
+    li.append((x,y))
+li.sort(key=lambda x:(x[1],x[0]))
 for i in li:
     print(*i)
