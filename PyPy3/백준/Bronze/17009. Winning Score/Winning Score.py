5917af97e5e95dc39d4239c37710cccf67c258c1
이효준 <148470980+123isi@users.noreply.github.com>
2025-09-22 14:02:16 +0900

    [Bronze IV] Title: Winning Score, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/17009. Winning Score/Winning Score.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+c=int(input())
+x=int(input())
+y=int(input())
+z=int(input())
+if (a*3)+(b*2)+c>(x*3)+(y*2)+z:
+    print("A")
+elif (a*3)++(b*2)+c<(x*3)+(y*2)+z:
+    print("B")
+else:
+    print("T")
