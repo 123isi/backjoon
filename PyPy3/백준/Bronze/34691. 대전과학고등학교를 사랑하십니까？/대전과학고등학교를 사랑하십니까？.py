3f81a2d738d4cb1c36d62c2a45996f1214b10ce9
이효준 <148470980+123isi@users.noreply.github.com>
2025-12-03 12:00:58 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 148 ms, Memory: 110708 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,11 @@
+a='a'
+while a!='end':
+    a=input()
+    if a=='end':
+        break
+    if a=='animal':
+        print('Panthera tigris')
+    elif a=='tree':
+        print('Pinus densiflora')
+    else:
+        print('Forsythia koreana')
