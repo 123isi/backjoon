d8fe8fe0f2758a5de9c83ce7536c4e65e16e8edc
이효준 <148470980+123isi@users.noreply.github.com>
2026-03-22 21:01:30 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT board_id,writer_id,title,price,if(status="DONE","거래완료",if(status="RESERVED","예약중","판매중")) as status from used_goods_board where created_date="2022-10-05" order by board_id desc
