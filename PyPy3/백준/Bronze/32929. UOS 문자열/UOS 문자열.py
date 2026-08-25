1ee23db7ed54c922f34661c31755e17488b2d6c2
이효준 <148470980+123isi@users.noreply.github.com>
2026-08-25 09:51:24 +0900

    refactor: 언어 폴더 표준화 Python3·PyPy3 → Python (#346) -BaekjoonHub

--- PyPy3/백준/Bronze/32929. UOS 문자열/UOS 문자열.py
+++
@@ -1,7 +0,0 @@
-a=int(input())
-if a%3==2:
-    print("O")
-elif a%3==1:
-    print("U")
-else:
-    print("S")
--- PyPy3/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++
@@ -1,8 +0,0 @@
-a=int(input())
-li=[]
-for i in range(a):
-    x,y=map(int,input().split())
-    li.append((x,y))
-li.sort(key=lambda x:(x[0],x[1]))
-for i in li:
-    print(*i)
